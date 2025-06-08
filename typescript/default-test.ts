80920820547dc8317200af2929b6cf3d7c042cf2
Yujie Liu <liu619666@163.com>
2025-06-09 00:53:00 +0800

    Test: 添加对New.ps1脚本的测试文件

--- typescript/default-test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileName: default-test.ts
- * @description: 无
- * @author: Yujie Liu
- * @date: 2025-06-08 22:00:24
- */
-export default function defaultTest() {
-    console.log('Hello, Typescript!');
-}
--- typescript/test-template-fix.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileName: test-template-fix.ts
- * @description: 无
- * @author: Yujie Liu
- * @date: 2025-06-08 21:50:06
- */
-export default function testTemplateFix() {
-    console.log(`'Hello, Typescript!`');
-}
